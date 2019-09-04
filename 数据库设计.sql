944ef496d80f952297a3d87bc03f02b058426f98
Grozta <1119284218@qq.com>
2019-09-04 12:21:52 +0800

    基本结束

--- 数据库设计.sql
+++ 数据库设计.sql
@@ -3,7 +3,7 @@ CREATE DATABASE PPF_system ;
 use PPF_system;
 CREATE TABLE Captain_And_Product(
     name VARCHAR(256),
-    id INT(7) ,
+    id INT ,
     sub_Project VARCHAR(256),
     project_Number INT,
     completion_Rate INT,
@@ -31,15 +31,15 @@ CREATE TABLE Employee(
     pay_Method VARCHAR(256),
     superior VARCHAR(256)
 );
-CREATE TABLE Project_Overview(
+CREATE TABLE Project_status(
     project_Number INT,
     project_Name VARCHAR(256),
     product_Manger VARCHAR(256),
     total_Project decimal(15,2),
-    completion_Rate TINYINT,
     used_Fund decimal(15,2),
-    captain VARCHAR(1024),
-    foreign key (product_Manger) references Employee (name)
+    completion_Rate TINYINT,
+    captain VARCHAR(1024)
+    -- foreign key (product_Manger) references Employee (name)
 );
 CREATE TABLE Project_Details(
     project_Number INT,
@@ -52,15 +52,14 @@ CREATE TABLE Project_Details(
     cur_Amount_Work TINYINT,
     cur_Apply_Fund decimal(15,2),
     completion_Rate TINYINT
-    foreign key (project_Number) references Project_Overview (project_Number),
-    foreign key (project_Name) references Project_Overview (project_Name),
-    foreign key (applyer_Id) references Employee (id),
-    foreign key (applyer_Name) references Employee (name),
-    foreign key (applyer_Pos) references Employee (pos),
-    foreign key (header_Number) references Employee (id),
-    foreign key (header) references Employee (name),
-    foreign key (completion_Rate) references Project_Overview (completion_Rate)
-
+    -- 5.5的版本的mysql不能使用外键 还有if语句
+    -- foreign key (project_Number) references Project_status (project_Number),
+    -- foreign key (project_Name) references Project_status (project_Name),
+    -- foreign key (applyer_Id) references Employee (id),
+    -- foreign key (applyer_Name) references Employee (name),
+    -- foreign key (applyer_Pos) references Employee (pos),
+    -- foreign key (header_Number) references Employee (id),
+    -- foreign key (header) references Employee (name),
 );
 -- Insert rows into table 'TableName'
 INSERT INTO Employee
@@ -70,3 +69,7 @@ VALUES
 ( null,"科奇","tyytdg35","131235238","队长",null,"名山","月薪","卢村"),
 ( null,"董太奇","t12y2g35","1235238","普通员工",null,null,"时薪","科奇");
 
+INSERT INTO Project_status
+VALUES
+(20150507,"山水家园","卢村",26632358.93,0,0,"李玉山 万科 董碧林"),
+(20190807,"盛世展厅","图论",25623465.71,37262424,54,"李玉山 万科 董碧林");
